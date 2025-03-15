d20a8aac91d47e9e2e3baa664074eaef1eb682bb
JACS 01 <jacs01@iMac-de-JACS.local>
2025-03-15 08:01:29 -0500

    Primer commit para Heroku

--- server.js
+++ server.js
@@ -193,3 +193,8 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("Servidor corriendo en http://localhost:3000");
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
